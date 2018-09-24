d292a3636a89ad164bd1f70c6f4fa9c8b77b2de4
Pon Muthu Selvam N <nponmuthuselvam@gmail.com>
2018-09-24 16:48:11 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'cx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'clothx';
+}
