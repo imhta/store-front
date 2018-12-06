eb978cf7706f5abfba3167f7b2d4d2b0aa1dc1a3
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-06 08:52:26 +0530

    added ci yml

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  config: {
+    config : {
+      apiKey: 'AIzaSyC25cgbnqtHtI_2JfDYfQOfjQZVYbAhnNI',
+      authDomain: 'clothxtest.firebaseapp.com',
+      databaseURL: 'https://clothxtest.firebaseio.com',
+      projectId: 'clothxtest',
+      storageBucket: 'clothxtest.appspot.com',
+      messagingSenderId: '275075175462'
+    }
+  },
+  algolia: {
+    appId: 'K5TY9WEM1N',
+    apiKey: 'c34f0706878bc2a520600b07f9587757'
+  }
+};
