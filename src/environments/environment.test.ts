70e2ae365db1029e1962b58b04cf11576a473410
Pon Muthu Selvam N <nponmuthuselvam@gmail.com>
2018-12-07 23:58:43 +0530

    Revert "Revert "algolia branch created commit""
    This reverts commit 3e9e538e9ace3d7f87d264bf5495144ad313f626.

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  config : {
+    apiKey: 'AIzaSyC25cgbnqtHtI_2JfDYfQOfjQZVYbAhnNI',
+    authDomain: 'clothxtest.firebaseapp.com',
+    databaseURL: 'https://clothxtest.firebaseio.com',
+    projectId: 'clothxtest',
+    storageBucket: 'clothxtest.appspot.com',
+    messagingSenderId: '275075175462'
+  }
+
+};
